646bd8b9141e26b9c41120281697fc2ea2ba0982
Arafathislam <islamarafath315@gmail.com>
2023-08-11 00:51:56 +0600

    all

---
+++ backend_api/myapp/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render
+from . import serializers
+from rest_framework import generics,permissions,pagination,viewsets
+from . import models
+# Create your views here.
+
+#Vendor
+class VendorList(generics.ListCreateAPIView):
+    queryset=models.Vendor.objects.all()
+    serializer_class=serializers.VendorSerializer
+    
+
+class VendorDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset=models.Vendor.objects.all()
+    serializer_class=serializers.VendorDetailsSerializer
+
+
+class ProductList(generics.ListCreateAPIView):
+    queryset=models.Product.objects.all()
+    serializer_class=serializers.ProductListSerializer
+    pagination_class=pagination.PageNumberPagination
+
+    # def get_queryset(self):
+    #     qs=super().get_queryset()
+    #     category=self.request.GET['category']
+    #     category=models.ProductCategory.objects.get(id=category)
+    #     qs=qs.filter(category=category)
+    #     return qs
+
+class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset=models.Product.objects.all()
+    serializer_class=serializers.ProductDetailsSerializer
+    
+
+
+# Customer
+
+class CustomerList(generics.ListCreateAPIView):
+    queryset=models.Customer.objects.all()
+    serializer_class=serializers.CustomerSerializer
+    pagination_class=pagination.PageNumberPagination
+    
+
+class CustomerDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset=models.Customer.objects.all()
+    serializer_class=serializers.CustomerDetailsSerializer
+
+#Order
+class OrderList(generics.ListCreateAPIView):
+    queryset=models.Order.objects.all()
+    serializer_class=serializers.OrderSerializer
+
+
+class OrderDetails(generics.ListAPIView):
+    
+    serializer_class=serializers.OrderDetailsSerializer
+
+    def get_queryset(self):
+        order_id=self.kwargs['pk']
+        order=models.Order.objects.get(id=order_id)
+        order_items=models.OrderItems.objects.filter(order=order)
+        return order_items
+
+
+class CustomerAddressViewSet(viewsets.ModelViewSet):
+    serializer_class=serializers.CustomerAddressSerializer
+    queryset=models.CustomerAddress.objects.all()
+
+class ProductRatingViewSet(viewsets.ModelViewSet):
+    serializer_class=serializers.ProductRatingSerializer
+    queryset=models.ProductReting.objects.all()
+
+#Category
+class CategoryList(generics.ListCreateAPIView):
+    queryset=models.ProductCategory.objects.all()
+    serializer_class=serializers.CategorySerializer
+    pagination_class=pagination.PageNumberPagination
+    
+
+class CategoryDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset=models.ProductCategory.objects.all()
+    serializer_class=serializers.CategoryDetailsSerializer
+
